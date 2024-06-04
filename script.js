f634d735e3fdbef30f4f555b0b063a4d9f7d4ad3
Mabdoul Mohammed <155731453+Mabdoul@users.noreply.github.com>
2024-06-04 15:12:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+$("section .content .text .aka").hover(function(){
+    $(".akatsuki").slideDown(900),
+    $(".aka").hide()
+})
+$("section .content .text .pa").hover(function(){
+    $(".pain").slideDown(900),
+    $(".pa").hide()
+})
+$("section .content .text .ko").hover(function(){
+    $(".konan").slideDown(900),
+    $(".ko").hide()
+})
+$("section .content .text .ita").hover(function(){
+    $(".itachi").slideDown(900),
+    $(".ita").hide()
+})
+    })
